7c40137a9ea04542f5baa84cf641890fe944890f
saliljoshi <salilsj1619@gmail.com>
2025-04-12 23:28:45 +0530

    Project Setup

---
+++ src/main/java/com/lld/ticketBooking/ticketBooking/TicketBookingApplication.java
@@ -0,0 +1,15 @@
+package com.lld.ticketBooking.ticketBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingApplication.class, args);
+
+		System.out.println("Hello world");
+	}
+
+}
